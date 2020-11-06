d82fbf3fa3a99a2b814178a8eb79816b329639ca
monica <monicausat2014@gmail.com>
2020-11-05 21:19:55 -0500

    commi monica

--- app/src/main/java/com/example/registration/MenuInicio.java
+++ app/src/main/java/com/example/registration/MenuInicio.java
@@ -4,17 +4,23 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.Menu;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MenuInicio extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu_inicio);
-
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_inicio);
 
         Button btnCuenta =(Button) findViewById(R.id.cuenta);
 
