cbcf41dca94a33ccaa6e48be71a3eca8c0e05c5c
dbcooper <marianolv11@gmail.com>
2020-11-18 18:33:12 -0500

    ModificacionDeCamaraParaTomarFoto

--- app/src/main/java/com/example/registration/MainActivity.java
+++ app/src/main/java/com/example/registration/MainActivity.java
@@ -1,12 +1,25 @@
 package com.example.registration;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.FileProvider;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -20,10 +33,17 @@ import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int REQUEST_PERMISSION_CAMERA = 100;
+    private static final int REQUEST_IMAGE_CAMERA = 101;
+    ImageView picture;
     private TextView userNombre,userEmail,userID;
     private CircleImageView userImg;
     private Button btnCerrarSesion, btnEliminarCta;
@@ -41,6 +61,10 @@ public class MainActivity extends AppCompatActivity {
         FirebaseUser user = mAuth.getCurrentUser();
         btnCerrarSesion = findViewById(R.id.btnLogout);
         btnEliminarCta = findViewById(R.id.btnEliminarCta);
+        ///Camara
+        picture = findViewById(R.id.userImagen);
+        ImageButton button = (ImageButton) findViewById(R.id.imagebotonCamara);
+        ////
 
         userEmail.setText(user.getEmail());
         userID.setText(user.getUid());
@@ -101,5 +125,94 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
+     ///boton de la camara
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
+                        goToCamera();
+                    } else {
+                        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.CAMERA},REQUEST_PERMISSION_CAMERA);
+                    }
+                } else {
+                    goToCamera();
+                }
+            }
+        });
+
+
     }
+    ///inicializacion de la camara
+    String currentPhotoPath;
+
+    private File createImageFile() throws IOException, IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "Backup_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+    static final int REQUEST_TAKE_PHOTO = 1;
+    public void tomarFoto() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,"com.example.android.fileprovider", photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults){
+        if(requestCode == REQUEST_PERMISSION_CAMERA){
+            if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                goToCamera();
+            } else {
+                Toast.makeText(this,"Necesita activar los permisos de la cámara",Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    //Método para mostrar vista previa en un imageview de la foto tomada
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(requestCode == REQUEST_IMAGE_CAMERA){
+            if(resultCode == Activity.RESULT_OK){
+                Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                picture.setImageBitmap(bitmap);
+                Log.i("TAG","Result=>"+bitmap);
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    private void goToCamera(){
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(cameraIntent.resolveActivity(getPackageManager())!=null){
+            startActivityForResult(cameraIntent,REQUEST_IMAGE_CAMERA);
+        }
+    }
+
+
 }
